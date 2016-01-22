7fe98bea2556dbe767269cf03cbe768eba9a645a
enihsyou <s1131234@gmail.com>
2016-01-22 19:02:13 +0800

    add some comments

--- 校门外的树.cpp
+++ 校门外的树.cpp
@@ -4,20 +4,20 @@ int main() {
 	int all = 0;
 	int row = 0;
 	int counts = 1;
-	cin >> all >> row;
+	cin >> all >> row; //Input the number of trees and how many rows datum
 	int arr[200] = {0};
-	for (int i = 0; i < row; i++) {
+	for (int i = 0; i < row; i++) { //input datum
 		cin >> arr[2 * i] >> arr[2 * i + 1];
-		if (arr[2 * i] > arr[2 * i + 1]) {
+		if (arr[2 * i] > arr[2 * i + 1]) {//change the array for which the second less than the first
 			int tmp = arr[2 * i];
 			arr[2 * i] = arr[2 * i + 1];
 			arr[2 * i + 1] = arr[2 * i];
 		}
 	}
 	int tree[200] = {0};
-	
 	int con = row;
 	int row2 = row;
+//每一次大循环从arr放下去一组数据到tree里，在小循环内每次检查当前数据与tree相应位置的范围大小。成功检测的组清零。
 	for (int j = 0; j < con; j++) {
 		tree[2 * j] = arr[2 * j], tree[2 * j + 1] = arr[2 * j + 1];
 		arr[2 * j] = 0, arr[2 * j + 1] = 0;
@@ -59,6 +59,7 @@ int main() {
 		}
 		row2 = row;
 	}
+// compute the sum
 	int sum = 0;
 	for (int i = 0; i < con; i++) {
 		if (arr[2 * i] == 0) continue;
@@ -69,6 +70,7 @@ int main() {
 		if (tree[2 * j] == 0) continue;
 		sum += (tree[2 * j + 1] - tree[2 * j] + 1);
 	}
+//print the sum
 	cout << (all + 1 - sum);
 	return 0;
 }
