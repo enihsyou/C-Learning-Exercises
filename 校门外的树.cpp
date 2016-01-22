ae484fc65235d46d121e449eac0fea815e8fb37c
enihsyou <s1131234@gmail.com>
2016-01-22 18:40:14 +0800

    bug fixed

--- 校门外的树.cpp
+++ 校门外的树.cpp
@@ -14,35 +14,57 @@ int main() {
 			arr[2 * i + 1] = arr[2 * i];
 		}
 	}
-	int tree[200] = {arr[0],arr[1]};
-	for (int i = 1; i < row; i++) {
-		bool cond = true;
-		if (arr[2 * i] <= tree[0] && arr[2 * i + 1] >= tree[1]) {
-			tree[0] = arr[2 * i];
-			tree[1] = arr[2 * i + 1];
-			cond = false;
+	int tree[200] = {0};
+	
+	int con = row;
+	int row2 = row;
+	for (int j = 0; j < con; j++) {
+		tree[2 * j] = arr[2 * j], tree[2 * j + 1] = arr[2 * j + 1];
+		arr[2 * j] = 0, arr[2 * j + 1] = 0;
+		while (row2 > 0) {
+			for (int i = j; i < con; i++) {
+				bool cond = true;
+				if (arr[2 * i] <= tree[2 * j] && arr[2 * i + 1] >= tree[2 * j + 1]) {
+					tree[2 * j] = arr[2 * i];
+					tree[2 * j + 1] = arr[2 * i + 1];
+					arr[2 * i] = 0;
+					arr[2 * i + 1] = 0;
+					cond = false;
+				}
+				if (arr[2 * i] <= tree[2 * j] && arr[2 * i + 1] >= tree[2 * j] && arr[2 * i + 1] <= tree[2 * j + 1]) {
+					tree[2 * j] = arr[2 * i];
+					arr[2 * i] = 0;
+					arr[2 * i + 1] = 0;
+					cond = false;
+				}
+				if (arr[2 * i] >= tree[2 * j] && arr[2 * i] <= tree[2 * j + 1] && arr[2 * i + 1] >= tree[2 * j + 1]) {
+					tree[2 * j + 1] = arr[2 * i + 1];
+					arr[2 * i] = 0;
+					arr[2 * i + 1] = 0;
+					cond = false;
+				}
+				if (arr[2 * i] >= tree[2 * j] && arr[2 * i + 1] <= tree[2 * j + 1]) {
+					continue;
+					cond = false;
+				}
+				//if (cond) {
+				//	tree[2 * (i - 1)] = arr[2 * i];
+				//	tree[2 * (i - 1) + 1] = arr[2 * i + 1];
+				//	counts++;
+				//}
+			}
+			row2--;
 		}
-		if (arr[2 * i] <= tree[0] && arr[2 * i + 1] >= tree[0] && arr[2 * i + 1] <= tree[1]) {
-			tree[0] = arr[2 * i];
-			cond = false;
-		}
-		if (arr[2 * i] >= tree[0] && arr[2 * i] <= tree[1] && arr[2 * i + 1] >= tree[1]) {
-			tree[1] = arr[2 * i + 1];
-			cond = false;
-		}
-		if (arr[2 * i] >= tree[0] && arr[2 * i + 1] <= tree[1]) {
-			continue;
-			cond = false;
-		}
-		if (cond) {
-			tree[2 * (i - 1)] = arr[2 * i];
-			tree[2 * (i - 1) + 1] = arr[2 * i + 1];
-			counts++;
-		}
+		row2 = row;
 	}
 	int sum = 0;
-	for (int i = 0; i < counts; i++) {
-		sum += (tree[2 * i + 1] - tree[2 * i] + 1);
+	for (int i = 0; i < con; i++) {
+		if (arr[2 * i] == 0) continue;
+		sum += (arr[2 * i + 1] - arr[2 * i] + 1);
+		
+	}
+	for (int j = 0; j < con; j++) {
+		sum += (tree[2 * j + 1] - tree[2 * j] + 1);
 	}
 	cout << (all - sum + 1);
 	return 0;
